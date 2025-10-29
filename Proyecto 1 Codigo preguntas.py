a28152b92e6c9cf2e36f7e5504a2f85d44bb9cc8
Noe <melvin.noe.777@gmail.com>
2025-10-29 15:35:21 -0600

    Add files via upload

---
+++ Proyecto 1 Codigo preguntas.py
@@ -0,0 +1,7 @@
+print("Tu Marca de Aviones es :  "+"\""+input("Dime el nombre de un Arbol: ")+" "+input("Dime tu color favorito: ")+"\"")
+print("Tu Marca de Carros es :  "+"\""+input("Dime el nombre de un Video juego: ")+" "+input("Dime tu color favorito: ")+"\"")
+print("Tu Marca de Ropa es :  "+"\""+input("Dime el nombre de un animal: ")+" "+input("Dime tu color favorito: ")+"\"")
+print("Tu marca de Bebidas es : "+"\""+input("Dime el nombre de un planeta: ")+" "+input("Dime tu color favorito: ")+"\"")
+print("Nombre Cliente : "+input("Ingresa tu nombre completo: ")+" "+input("Ingresa tu Apellido completo:"))
+# Se puede hacer en una sola linea
+# la barra inverida siempre va en comillas "\"
---
+++ Proyecto 1 crear nombre de marca .py
@@ -0,0 +1,13 @@
+# Crear un codigo que haga dos preguntas
+# y que luego las muestre en pantalla las dos palabras conbinadas
+
+print("Bienvenido a el programa para crear el nombre de su Marca de Aviones ")
+print()
+Primer_nombre = input("Por favor ingresa el primer nombre de la marca de \"Aviones\",piensa en algo Interezante ")
+Segundo_nombre = input("Por favor ingresa el segundo nombre de la marca de \"Aviones\",piensa en algo Genial ")
+print()
+print("Felicidades, el nombre de su marca de Aviones es: "+"\""+Primer_nombre +" "+Segundo_nombre+"\"")
+
+# La barra invertida sirve para poder poner comillas en las comillas
+
+# Se puede utilizar un print () vacio para poner una linea
